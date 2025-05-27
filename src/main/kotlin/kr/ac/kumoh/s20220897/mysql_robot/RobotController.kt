dedb5d5306078fc9c4abd604f2cacf9f91915bd7
이준민 <s01094631883@gmail.com>
2025-05-27 14:42:27 +0900

    데이터 베이스 연결

---
+++ src/main/kotlin/kr/ac/kumoh/s20220897/mysql_robot/RobotController.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220897.mysql_robot
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+
+class RobotController {
+    @GetMapping("/")
+    fun welcome(): String {
+             return "Welcome to Robot Server"
+    }
+}
