74ebb9fd57ccc15e36b06d849ea80963337856e0
이준민 <s01094631883@gmail.com>
2025-05-28 14:19:50 +0900

    데이터 베이스 연결

--- src/main/kotlin/kr/ac/kumoh/s20220897/mysql_robot/RobotController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220897/mysql_robot/RobotController.kt
@@ -11,3 +11,4 @@ class RobotController {
              return "Welcome to Robot Server"
     }
 }
+
---
+++ src/main/kotlin/kr/ac/kumoh/s20220897/mysql_robot/robot.kt
@@ -0,0 +1,7 @@
+package kr.ac.kumoh.s20220897.mysql_robot
+
+import jakarta.persistence.Entity
+
+@Entity
+
+data class robot()
